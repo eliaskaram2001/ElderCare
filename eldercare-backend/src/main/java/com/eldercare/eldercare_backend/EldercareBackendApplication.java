cfef56d4e2b83cef925e9cc6e4528c58431de7f1
eliaskaram2001 <160174996+eliaskaram2001@users.noreply.github.com>
2025-11-24 22:11:36 -0500

    SpringBoot Skeleton
    This is the SpringBoot Backend for ElderCare

---
+++ eldercare-backend/src/main/java/com/eldercare/eldercare_backend/EldercareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eldercare.eldercare_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EldercareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EldercareBackendApplication.class, args);
+	}
+
+}
---
+++ eldercare-backend/src/test/java/com/eldercare/eldercare_backend/EldercareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eldercare.eldercare_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EldercareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
